8fd047b933e01d8dd971053aa2ae2bd9e628f2e3
kinduff <kinduff@protonmail.com>
2021-10-24 17:18:31 +0200

    Adds lastfm generator

---
+++ _plugins/last_fm.rb
@@ -0,0 +1,29 @@
+class LastFm < Jekyll::Generator
+  safe true
+
+  def generate(site)
+    config = site.config['jekyll_last_fm']
+    return unless config
+
+    user = config['user']
+    api_key = config['api_key']
+    limit = config['limit']
+    return unless user && api_key
+
+    last_fm_url = "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=#{user}&api_key=#{api_key}&limit=#{limit}&format=json"
+    last_fm_json = JSON.parse(URI.open(last_fm_url).read)
+
+    return unless last_fm_json['recenttracks']['track']
+
+    site.data['last_fm'] = []
+    last_fm_json['recenttracks']['track'].each do |track|
+      site.data['last_fm'] << {
+        'title' => track['name'],
+        'url' => track['url'],
+        'artist' => track['artist']['#text'],
+        'album' => track['album']['#text'],
+        'image' => track['image'][0]['#text']
+      }
+    end
+  end
+end
